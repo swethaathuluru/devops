5d13f9bbe9e7f95b2b53ce68334a32e258e4afbf
swethaathuluru <swethaathuluru7@gmail.com>
2021-06-05 09:55:29 +0530

    devops grade

---
+++ src/main/java/com/example/SpringBootDevopsApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDevopsApplication.class, args);
+		System.out.println("hello devops");
+	}
+
+}
